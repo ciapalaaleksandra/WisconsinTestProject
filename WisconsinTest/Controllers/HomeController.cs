48c26d135fee84c5c0a03f4c0cac96d59c06bad9
ciapalaaleksandra <alekcia869@student.polsl.pl>
2018-04-01 21:40:21 +0200

    login

--- WisconsinTest/Controllers/HomeController.cs
+++ WisconsinTest/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WisconsinTest.Models;
+using System.Data.Entity;
 
 namespace WisconsinTest.Controllers
 {
@@ -26,5 +28,37 @@ namespace WisconsinTest.Controllers
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Users u)
+        {
+            
+            using (WiscounsinTestDatabaseEntities db = new WiscounsinTestDatabaseEntities())
+            {
+                
+                if (u.Login != null && u.Passowrd != null)
+                {
+                    var v = db.Users.Where(a => a.Login == u.Login && a.Passowrd == u.Passowrd).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["LogedUserID"] = v.UserId.ToString();
+                        Session["LogedUserLogin"] = v.Login.ToString();
+                        
+                        return RedirectToAction("AfterLogin");
+                    }
+                }
+            }
+            ViewBag.Error = "Logowanie nie powiodlo sie.";
+            return View(u);
+
+        }
+
+
     }
 }
